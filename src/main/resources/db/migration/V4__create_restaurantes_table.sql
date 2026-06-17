39d885bfa46f6fba2743ceda3ff22f9ac485b539
Rafael.Bezerra <88746416+Rafazls@users.noreply.github.com>
2026-06-17 17:48:46 -0300

    feat: Adicionando migration de restaurante (#12)

---
+++ src/main/resources/db/migration/V4__create_restaurantes_table.sql
@@ -0,0 +1,34 @@
+-- Sequence
+CREATE SEQUENCE restaurantes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+-- Tabela restaurantes
+CREATE TABLE restaurantes (
+    id BIGINT NOT NULL DEFAULT nextval('restaurantes_id_seq'),
+
+    nome VARCHAR(150) NOT NULL,
+    tipo_cozinha VARCHAR(100),
+    horario_funcionamento VARCHAR(100),
+
+    dono_id BIGINT NOT NULL,
+    endereco_id BIGINT,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_restaurantes PRIMARY KEY (id),
+
+    CONSTRAINT fk_restaurante_dono
+        FOREIGN KEY (dono_id)
+            REFERENCES usuarios(id)
+            ON DELETE RESTRICT,
+
+    CONSTRAINT fk_restaurante_endereco
+        FOREIGN KEY (endereco_id)
+            REFERENCES enderecos(id)
+            ON DELETE SET NULL
+);
